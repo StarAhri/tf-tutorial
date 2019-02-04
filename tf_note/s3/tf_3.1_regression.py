e684b753db6c39fb089a8ff65c775edfcae2be2e
Ahri <StarAhri@qq.com>
2019-02-04 12:11:17 +0800

    'fix'

--- tf_note/s3/tf_3.1_regression.py
+++ tf_note/s3/tf_3.1_regression.py
@@ -1,4 +1,12 @@
 import tensorflow as tf
 import numpy as np
-import matplotlib.pylot as plt
+import matplotlib.pyplot as plt
 
+
+x_data=np.linspace(-0.5,0.5,200)[:,np.newaxis]
+noise=np.random.normal(0,0.02,x_data.shape)
+y_data=np.square(x_data)+noise
+
+# 定义两个placeholder
+x=tf.placeholder(tf.float32,[None,1])
+y=tf.placeholder(tf.float32,[None,1])
